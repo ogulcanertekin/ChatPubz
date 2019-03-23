4da890d720ab6badd4224e6471296d3e0c540256
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-03-23 19:41:32 +0300

    Socket-io - Redis Adapter

---
+++ src/socketApi.js
@@ -0,0 +1,21 @@
+const socketio = require('socket.io');
+
+const io = socketio();
+
+const socketApi = {
+    io:io
+};
+
+// Redis Adapter
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
+io.on('connection',socket=>{
+    console.log('a user logged in');
+});
+
+module.exports = socketApi;
